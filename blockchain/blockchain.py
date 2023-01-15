6072b88e6c92ec0f06f23232c2071dc30f5755d6
ryuichi kurihara <ryouichi.kurihara.628@gmail.com>
2023-01-15 22:33:56 +0900

    配置の変更のため削除

--- blockchain/blockchain.py
+++
@@ -1,8 +0,0 @@
-class BlockChain(object):
-    """
-    ブロックチェーンの構造や機能を含んだクラス
-    """
-    def __init__(self):
-        self.transaction_pool = {"transactions": []}
-        self.block = {"blocks": []}
-    
