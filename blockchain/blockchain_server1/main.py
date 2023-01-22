5926ebb5215bd93e2224a47ee9d3c41e6de5046a
ryuichi kurihara <ryouichi.kurihara.628@gmail.com>
2023-01-22 23:32:18 +0900

    サーバー１の修正

--- blockchain/blockchain_server1/main.py
+++ blockchain/blockchain_server1/main.py
@@ -1,5 +1,7 @@
 import os
 import sys
+import json
+
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from fastapi import FastAPI
@@ -7,7 +9,12 @@ from blockchain import BlockChain
 from transaction import Transaction
 
 app = FastAPI()
+print(app.root_path)
 blockchain = BlockChain()
+
+with open("./api_list_1.json") as f:
+    api_list = json.load(f)
+    blockchain.add_server_list(api_list["server_list"])
 
 @app.get("/transaction_pool")
 def get_transaction():
@@ -18,7 +25,7 @@ def get_transaction():
 
 @app.get("/chain")
 def get_chain():
-    """チェーンをブラウザに表示させる
+    """チェーンをブラウザ上に表示させる
     """
     return blockchain.chain
 
@@ -37,3 +44,8 @@ def create_block(creator: str):
     """
     blockchain.create_new_block(creator)
     return {"message": "New Block is Created."}
+
+@app.post("/receive_transaction")
+def receive_transaction(transaction :Transaction):
+    blockchain.add_transaction_pool(transaction)
+    return { "message" : "Broadcast Transaction is success."}
