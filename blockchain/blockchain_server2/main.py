3259a403ea548d4c5d1b444546a1e13caa964d4b
ryuichi kurihara <ryouichi.kurihara.628@gmail.com>
2023-01-26 21:25:16 +0900

    ブロックチェーンのメインファイルを修正

--- blockchain/blockchain_server2/main.py
+++ blockchain/blockchain_server2/main.py
@@ -7,6 +7,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from fastapi import FastAPI
 from blockchain import BlockChain
 from transaction import Transaction
+from chain import Chain
 
 app = FastAPI()
 print(app.root_path)
@@ -35,6 +36,7 @@ def post_transaction_pool(transaction :Transaction):
     トランザクションをトランザクションプールに追加する
     """
     blockchain.add_transaction_pool(transaction)
+    blockchain.broadcast_transaction(transaction)
     return { "message" : "Transaction is posted."}
 
 @app.get("/create_block/{creator}")
@@ -43,9 +45,15 @@ def create_block(creator: str):
     ブロックの生成処理を行う
     """
     blockchain.create_new_block(creator)
+    blockchain.broadcast_chain(blockchain.chain)
     return {"message": "New Block is Created."}
 
 @app.post("/receive_transaction")
 def receive_transaction(transaction :Transaction):
     blockchain.add_transaction_pool(transaction)
     return { "message" : "Broadcast Transaction is success."}
+
+@app.post("/receive_chain")
+def receive_chain(chain: Chain):
+    blockchain.replace_chain(chain)
+    return { "message" : "Broadcast Chain is success."}
